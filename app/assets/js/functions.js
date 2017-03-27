686980ba1f386da30629a03f0784647e3454fe25
ColaChan <greenzorro@163.com>
2017-03-28 02:24:43 +0800

    no message

--- app/assets/js/functions.js
+++ app/assets/js/functions.js
@@ -11,15 +11,6 @@
 
 		// 测试组件
 		test: function () {
-			var TestComponent = React.createClass({
-				render: function() {
-					return <p className={this.props.class1}>the content is: {this.props.sth} + {this.props.sth2}</p>;
-				}
-			});
-			ReactDOM.render(
-				<TestComponent class1="testClass" sth="nothing" sth2="nothing2" />,
-				document.getElementById('example')
-			);
 		}
 
 	}
